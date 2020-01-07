2a8e35478ce4c542c3eb6b5e3b930079c8df318a
Jomir Uddin <jomir.uddin112@gmail.com>
2020-01-07 14:45:27 -0500

    layout created

--- app.rb
+++ app.rb
@@ -1,63 +1,71 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require './environments'
 require 'pg'
 require './models'
 
 set :port, 6060
 
-set :database, {adapter: 'postgresql', database: 'finalproject'}
+set :database, {adapter: 'postgresql', database: 'finalproject',username: 'postgres', password: ENV['POSTGRES_PW']}
 
 enable :sessions
 
 get '/' do
-  erb :home
+    erb :home
+end
+
+post '/' do
+    @post = Post.new(params[:content])
+    if @post.valid?
+        pp @post
+        @post.save
+        redirect '/'
+    else
+        flash[:errors] = @post.errors.full_messages
+        redirect '/signup'
+    end
 end
 
 get '/login' do
-  erb :login
+    erb :login
 end
 
-post '/login'do
-  user = User.find_by(email: params[:email])
-  given_password = params[:password]
-  if user.password == given_password
-    session[:user_id] = user.id 
-    redirect '/'
-  else 
-    flash[:error] = "Correct email, but wrong password. Did you mean #{user.password}
-    only "
-    end 
+post '/login' do
+    user = User.find_by(email: params[:email])
+    given_password = params[:password]
+    if user.password == given_password
+        session[:user_id] = user.id
+        session[:user_name] = user.name
+        redirect '/profile'
+    else
+        flash[:error] = "Correct email but wrong password. did you mean #{user.password} 
+        \ Only use this password if this is your account"
+        redirect '/login'
+    end
 end
-
-get '/logout'do
-  session.clear
-  redirect '/login'
-end
-
-post '/logout' do 
-  session.clear
-  p "User Logged out Successfully"
-  redirect '/login'
-end 
 
 get '/signup' do
-  erb :signup
+    erb :signup
 end
 
 post '/signup' do
-  @user =User.new(params[:user])
-  if @user.valid?
-    @user.save
-    redirect '/profile'
-  else
-    flash[:error] = @user.errors.full_message
-    redirect '/signup'
-  end
-  p para
+    @user = User.new(params[:user])
+    if @user.valid?
+        puts @user
+        @user.save
+        redirect '/'
+    else
+        flash[:errors] = @user.errors.full_messages
+        redirect '/signup'
+    end
 end
 
 get '/profile' do
-  redirect '/' unless session[:user_id]
-  erb :profile
+    if session[:user_id] 
+        erb :profile
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect '/'
 end
