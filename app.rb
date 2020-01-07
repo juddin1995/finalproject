b7713bd415fe6355ea0e75c41aae72b6af994a3a
Jomir Uddin <jomir.uddin112@gmail.com>
2020-01-07 11:22:41 -0500

    Pre Postgres work

--- app.rb
+++ app.rb
@@ -1,6 +1,63 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'pg'
+require './models'
 
-class Post < ActiveRecord::Base
+set :port, 6060
+
+set :database, {adapter: 'postgresql', database: 'blogusers'}
+
+enable :sessions
+
+get '/' do
+  erb :home
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login'do
+  user = User.find_by(email: params[:email])
+  given_password = params[:password]
+  if user.password == given_password
+    session[:user_id] = user.id 
+    redirect '/'
+  else 
+    flash[:error] = "Correct email, but wrong password. Did you mean #{user.password}
+    only "
+    end 
+end
+
+get '/logout'do
+  session.clear
+  redirect '/login'
+end
+
+post '/logout' do 
+  session.clear
+  p "User Logged out Successfully"
+  redirect '/login'
+end 
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  @user =User.new(params[:user])
+  if @user.valid?
+    @user.save
+    redirect '/profile'
+  else
+    flash[:error] = @user.errors.full_message
+    redirect '/signup'
+  end
+  p para
+end
+
+get '/profile' do
+  redirect '/' unless session[:user_id]
+  erb :profile
 end
--- models.rb
+++ models.rb
@@ -3,3 +3,7 @@ class User < ActiveRecord::Base
     validates :email, :username, uniqueness: true
     validates :password, length: {minimum: 5, max: 8}
 end
+
+class Post < ActiveRecord::Base
+    validates :content, length: {maximum: 255}
+end
